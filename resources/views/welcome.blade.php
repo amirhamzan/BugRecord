4f578624e2cfa0359e45eb4c39247d6140b61ec4
Amir Hamzan <62345746+amirhamzan@users.noreply.github.com>
2022-02-07 10:13:34 +0800

    composer debugbar, h1 heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Laravel</title>
 
+        <h1>amir testing</h1>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
