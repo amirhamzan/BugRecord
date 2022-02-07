20f8e7a6ee2f1322e2f99674439c7d3dafa47bf2
Amir Hamzan <62345746+amirhamzan@users.noreply.github.com>
2022-02-07 12:29:12 +0800

    update h3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <h2>update mygym. branch main</h2>
         <h3>update h3 yg ketiga</h3>
 
+        <h4>ni update bugrecord h4</h4>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
